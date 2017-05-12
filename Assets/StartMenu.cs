96e0caa4fc8dcffc1346115aa5a75850e39eac79
PCFreeman <freemanfrank96@gmail.com>
2017-05-12 10:51:15 -0700

    4

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+    [SerializeField]
+   private GameObject Newcam;
+
+    [SerializeField]
+    private GameObject Player;
+
+    [SerializeField]
+    private GameObject Menu;
+
+    [SerializeField]
+    private GameObject Ingame;
+
+
+    public void Play()
+    {
+        Newcam.SetActive(false);
+        Player.SetActive(true);
+        Ingame.SetActive(true);
+        Menu.SetActive(false);
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
